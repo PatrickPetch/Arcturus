c458005723e6af63d0661e25302aa3d736b8286c
Petchie <53936910+PatrickPetch@users.noreply.github.com>
2020-08-18 19:46:40 +0800

    Update Navbar

--- src/App.js
+++ src/App.js
@@ -6,15 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <NavBar />
       HelloWorld
-=======
-      <p>
-          Hello World :)
-          Welcome to SEDS - NTU
-      </p>
->>>>>>> 82e1e96ea4c013e9743fa0e396b80af5a85a73e5
     </div>
   );
 }
