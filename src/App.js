aded466491f7ba7cfc97f16ab4636f8b4ff7425e
Petchie <53936910+PatrickPetch@users.noreply.github.com>
2020-08-18 19:45:07 +0800

    Update NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.scss';
+import NavBar from './components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-        HelloWorld
+      <NavBar />
+      HelloWorld
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Nav, NavDropdown, Navbar } from 'react-bootstrap'
+
+function NavBar() {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="#home">SEDS-NTU</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto"></Nav>
+                    <Nav>
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#news">News</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <NavDropdown title="Project" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#new-dawn">New Dawn</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#astrobiology">Astrobiology</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#events">Events</Nav.Link>
+                        <Nav.Link href="#team">Team</Nav.Link>
+                        <Nav.Link href="#join-is">Join Us</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    )
+}
+
+export default NavBar
